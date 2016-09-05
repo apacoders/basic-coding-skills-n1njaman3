0df6688b8fec7bc0b9b4feab899ead2b507ea93d
tim <tim@192.168.150.196>
2016-09-05 10:45:08 -0600

    Added comment

---
+++ src/assignment1/Assignment1.java
@@ -0,0 +1,56 @@
+package assignment1;
+
+/**
+ * Instructions for this assignment.  Download this template from GitHub, fill in
+ * the author comment below with your name and implement the requirements below.
+ * 
+ * If code is turned in and cannot be compiled, you will receive 0 points.
+ * If a requirement is not implemented, you will receive 0 points.
+ * Requirements are worth 50 points each.
+ * 
+ * @author myname
+ * 
+ * Requirements for main method
+ * 
+ * REQUIREMENT 1 
+ * 
+ * Print numbers 1 through 5 on screen using a for loop
+ * 
+ * REQUIREMENT 2 
+ * 
+ * Print numbers 10 down to -1 on screen using a while loop.  The while loop
+ * must use a boolean variable as the argument for the while statement.
+ * 
+ * REQUIREMENT 3 
+ * 
+ * Call the static method CallMeIf in a for loop with the arguments 1 through 6.
+ * Print the results of the CallMeIf method on the screen
+ * (see the REQUIREMENTS in that method)
+ * 
+ * REQUIREMENT 4 
+ * 
+ * Call the static method CallMeSwitch in a for loop with the arguments 1 through 6.
+ * Print the results of the CallMeSwitch method on the screen
+ * (see the REQUIREMENTS in that method)
+ * 
+ * DESIGN_REQUIREMENT
+ * 
+ * This must be handed in on paper - it must be neat!
+ * 
+ * Show a flow chart of the loop in REQUIREMENT 2
+ * 
+ * TEST_REQUIREMENT
+ * 
+ * Show a trace of REQUIREMENT 1
+ * 
+ */
+public class Assignment1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ src/assignment1/NewClass.java
@@ -0,0 +1,39 @@
+package assignment1;
+
+/**
+ *
+ * Requirements for class:  NewClass
+ * 
+ * REQUIREMENT 5:
+ * 
+ * There must be a static method called CallMeIf.
+ * 
+ * REQUIREMENT 6:
+ * 
+ * There must be a static method called CallMeSwitch
+ * 
+ * REQUIREMENT 7:
+ * 
+ * The static method CallMe must take an integer from one to five.
+ * Return a string that contains:
+ * 
+ * "one", "two", "three", "four", "five", or "Unknown"
+ * 
+ * if the integer passed to the method is 1, then the method will return "one"
+ * if the integer passed to the method is 2, then the method will return "two"
+ * if the integer passed to the method is 3, then the method will return "three" 
+ * if the integer passed to the method is 4, then the method will return "four" 
+ * if the integer passed to the method is 5, then the method will return "five" 
+ * if the integer passed is not 1,2,3,4, or 5 - then the method will return "Unknown"
+ * 
+ * this algorithm must be implemented using if/else statements
+ * 
+ * REQUIREMENT 8:
+ * 
+ * The static method CallMeSwitch does the same thing as REQUIREMENT 6; however, it
+ * is implemented using a switch statement.
+ * 
+ */
+public class NewClass {
+    
+}
